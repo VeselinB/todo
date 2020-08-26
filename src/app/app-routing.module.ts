c88d304c98eeac7d71a43af9ca3953b2b6ebe55f
VeselinB <veselin.budinov@gmail.com>
2020-08-26 12:36:35 +0300

    issue with update one2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'todos',
     loadChildren: () => import('./todo/todo.module').then((m) => m.TodoModule),
   },
   {
